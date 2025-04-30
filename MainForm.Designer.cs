d0e7398ff25e7417ed0c6184c0ab2a42d5848b8b
Miguel Alarcón <alarconbenjamin2001@gmail.com>
2025-04-30 01:24:04 -0400

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Sort_er
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.Text = "sort-er";
+            this.Size = new System.Drawing.Size(700, 550);
+            this.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+
+            System.Windows.Forms.Panel mainPanel = new System.Windows.Forms.Panel
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                Padding = new System.Windows.Forms.Padding(20)
+            };
+            this.Controls.Add(mainPanel);
+
+            System.Windows.Forms.Label titleLabel = new System.Windows.Forms.Label
+            {
+                Text = "srt-er",
+                Font = new System.Drawing.Font("Segoe UI", 18, System.Drawing.FontStyle.Bold),
+                ForeColor = System.Drawing.Color.FromArgb(44, 62, 80),
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 50,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+            };
+            mainPanel.Controls.Add(titleLabel);
+
+            System.Windows.Forms.GroupBox dirPanel = new System.Windows.Forms.GroupBox
+            {
+                Text = "Directory selection",
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 80,
+                Padding = new System.Windows.Forms.Padding(10),
+                Font = new System.Drawing.Font("Segoe UI", 10)
+            };
+            mainPanel.Controls.Add(dirPanel);
+
+            directoryTextBox = new System.Windows.Forms.TextBox
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                Font = new System.Drawing.Font("Segoe UI", 10)
+            };
+            dirPanel.Controls.Add(directoryTextBox);
+
+            System.Windows.Forms.Button browseButton = new System.Windows.Forms.Button
+            {
+                Text = "Browse",
+                Dock = System.Windows.Forms.DockStyle.Right,
+                Width = 100,
+                BackColor = System.Drawing.Color.FromArgb(52, 152, 219),
+                ForeColor = System.Drawing.Color.White,
+                Font = new System.Drawing.Font("Segoe UI", 9, System.Drawing.FontStyle.Bold),
+                Cursor = System.Windows.Forms.Cursors.Hand,
+                Name = "browseButton"
+            };
+            browseButton.Click += BrowseButton_Click;
+            dirPanel.Controls.Add(browseButton);
+
+            System.Windows.Forms.GroupBox optionsPanel = new System.Windows.Forms.GroupBox
+            {
+                Text = "Options",
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 90,
+                Padding = new System.Windows.Forms.Padding(10),
+                Font = new System.Drawing.Font("Segoe UI", 10),
+                Top = dirPanel.Bottom + 10
+            };
+            mainPanel.Controls.Add(optionsPanel);
+
+            System.Windows.Forms.CheckBox subdirsCheckbox = new System.Windows.Forms.CheckBox
+            {
+                Text = "Include subdirectories",
+                Checked = true,
+                Location = new System.Drawing.Point(10, 25),
+                AutoSize = true
+            };
+            subdirsCheckbox.CheckedChanged += (s, e) => { includeSubdirectories = subdirsCheckbox.Checked; };
+            optionsPanel.Controls.Add(subdirsCheckbox);
+
+            System.Windows.Forms.CheckBox uppercaseCheckbox = new System.Windows.Forms.CheckBox
+            {
+                Text = "Folder names in capital letters",
+                Checked = true,
+                Location = new System.Drawing.Point(10, 50),
+                AutoSize = true
+            };
+            uppercaseCheckbox.CheckedChanged += (s, e) => { useUppercase = uppercaseCheckbox.Checked; };
+            optionsPanel.Controls.Add(uppercaseCheckbox);
+
+            System.Windows.Forms.Panel buttonPanel = new System.Windows.Forms.Panel
+            {
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 50,
+                Top = optionsPanel.Bottom + 10
+            };
+            mainPanel.Controls.Add(buttonPanel);
+
+            System.Windows.Forms.Button organizeButton = new System.Windows.Forms.Button
+            {
+                Text = "Organize files",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                BackColor = System.Drawing.Color.FromArgb(46, 204, 113),
+                ForeColor = System.Drawing.Color.White,
+                Font = new System.Drawing.Font("Segoe UI", 12, System.Drawing.FontStyle.Bold),
+                Cursor = System.Windows.Forms.Cursors.Hand,
+                Name = "organizeButton"
+            };
+            organizeButton.Click += OrganizeButton_Click;
+            buttonPanel.Controls.Add(organizeButton);
+
+            System.Windows.Forms.GroupBox progressPanel = new System.Windows.Forms.GroupBox
+            {
+                Text = "Progress",
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 80,
+                Padding = new System.Windows.Forms.Padding(10),
+                Font = new System.Drawing.Font("Segoe UI", 10),
+                Top = buttonPanel.Bottom + 10
+            };
+            mainPanel.Controls.Add(progressPanel);
+
+            progressBar = new System.Windows.Forms.ProgressBar
+            {
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 25
+            };
+            progressPanel.Controls.Add(progressBar);
+
+            statusLabel = new System.Windows.Forms.Label
+            {
+                Text = "Waiting for operation...",
+                Dock = System.Windows.Forms.DockStyle.Bottom,
+                Height = 20
+            };
+            progressPanel.Controls.Add(statusLabel);
+
+            System.Windows.Forms.GroupBox statsPanel = new System.Windows.Forms.GroupBox
+            {
+                Text = "Statistics",
+                Dock = System.Windows.Forms.DockStyle.Top,
+                Height = 100,
+                Padding = new System.Windows.Forms.Padding(10),
+                Font = new System.Drawing.Font("Segoe UI", 10),
+                Top = progressPanel.Bottom + 10
+            };
+            mainPanel.Controls.Add(statsPanel);
+
+            System.Windows.Forms.TableLayoutPanel statsGrid = new System.Windows.Forms.TableLayoutPanel
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                RowCount = 3,
+                ColumnCount = 2
+            };
+            statsPanel.Controls.Add(statsGrid);
+
+            statsGrid.Controls.Add(new System.Windows.Forms.Label { Text = "Total files:", TextAlign = System.Drawing.ContentAlignment.MiddleLeft }, 0, 0);
+            totalFilesLabel = new System.Windows.Forms.Label { Text = "0", Font = new System.Drawing.Font("Segoe UI", 9, System.Drawing.FontStyle.Bold), TextAlign = System.Drawing.ContentAlignment.MiddleLeft };
+            statsGrid.Controls.Add(totalFilesLabel, 1, 0);
+
+            statsGrid.Controls.Add(new System.Windows.Forms.Label { Text = "Processed files:", TextAlign = System.Drawing.ContentAlignment.MiddleLeft }, 0, 1);
+            processedFilesLabel = new System.Windows.Forms.Label { Text = "0", Font = new System.Drawing.Font("Segoe UI", 9, System.Drawing.FontStyle.Bold), TextAlign = System.Drawing.ContentAlignment.MiddleLeft };
+            statsGrid.Controls.Add(processedFilesLabel, 1, 1);
+
+            statsGrid.Controls.Add(new System.Windows.Forms.Label { Text = "Folders created:", TextAlign = System.Drawing.ContentAlignment.MiddleLeft }, 0, 2);
+            createdFoldersLabel = new System.Windows.Forms.Label { Text = "0", Font = new System.Drawing.Font("Segoe UI", 9, System.Drawing.FontStyle.Bold), TextAlign = System.Drawing.ContentAlignment.MiddleLeft };
+            statsGrid.Controls.Add(createdFoldersLabel, 1, 2);
+
+            System.Windows.Forms.GroupBox logPanel = new System.Windows.Forms.GroupBox
+            {
+                Text = "Activity Log",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                Padding = new System.Windows.Forms.Padding(10),
+                Font = new System.Drawing.Font("Segoe UI", 10),
+                Top = statsPanel.Bottom + 10
+            };
+            mainPanel.Controls.Add(logPanel);
+
+            logTextBox = new System.Windows.Forms.RichTextBox
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                BackColor = System.Drawing.Color.FromArgb(250, 250, 250),
+                Font = new System.Drawing.Font("Console", 9),
+                ReadOnly = true
+            };
+            logPanel.Controls.Add(logTextBox);
+
+            mainPanel.Controls.SetChildIndex(logPanel, 0);
+            mainPanel.Controls.SetChildIndex(statsPanel, 0);
+            mainPanel.Controls.SetChildIndex(progressPanel, 0);
+            mainPanel.Controls.SetChildIndex(buttonPanel, 0);
+            mainPanel.Controls.SetChildIndex(optionsPanel, 0);
+            mainPanel.Controls.SetChildIndex(dirPanel, 0);
+            mainPanel.Controls.SetChildIndex(titleLabel, 0);
+
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        }
+
+        #endregion
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,270 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sort_er
+{
+    public partial class MainForm : Form
+    {
+        private string selectedDirectory = "";
+        private bool includeSubdirectories = true;
+        private bool useUppercase = true;
+        private int totalFiles = 0;
+        private int processedFiles = 0;
+        private int createdFolders = 0;
+
+        private TextBox directoryTextBox;
+        private ProgressBar progressBar;
+        private Label statusLabel;
+        private Label totalFilesLabel;
+        private Label processedFilesLabel;
+        private Label createdFoldersLabel;
+        private RichTextBox logTextBox;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            selectedDirectory = Environment.CurrentDirectory;
+            directoryTextBox.Text = selectedDirectory;
+            Log("Application started. Select a directory and click on 'Organize files'.");
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.SelectedPath = selectedDirectory;
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedDirectory = folderDialog.SelectedPath;
+                    directoryTextBox.Text = selectedDirectory;
+                    Log($"Selected directory: {selectedDirectory}");
+                }
+            }
+        }
+
+        private async void OrganizeButton_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(selectedDirectory))
+            {
+                MessageBox.Show("Please select a valid directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Reiniciar estadísticas
+            totalFiles = 0;
+            processedFiles = 0;
+            createdFolders = 0;
+            totalFilesLabel.Text = "0";
+            processedFilesLabel.Text = "0";
+            createdFoldersLabel.Text = "0";
+            progressBar.Value = 0;
+
+            // Desactivar controles durante el proceso
+            EnableControls(false);
+
+            // Ejecutar organización en segundo plano
+            await Task.Run(() => OrganizeFiles());
+
+            // Reactivar controles
+            EnableControls(true);
+        }
+
+        private void EnableControls(bool enable)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<bool>(EnableControls), enable);
+                return;
+            }
+
+            directoryTextBox.Enabled = enable;
+            foreach (Control control in this.Controls.Find("browseButton", true))
+                control.Enabled = enable;
+            foreach (Control control in this.Controls.Find("organizeButton", true))
+                control.Enabled = enable;
+        }
+
+        private void Log(string message)
+        {
+            if (logTextBox.InvokeRequired)
+            {
+                logTextBox.Invoke(new Action<string>(Log), message);
+                return;
+            }
+
+            string timestamp = DateTime.Now.ToString("[HH:mm:ss]");
+            logTextBox.AppendText($"{timestamp} {message}\r\n");
+            logTextBox.ScrollToCaret();
+        }
+
+        private void UpdateStatus(string message)
+        {
+            if (statusLabel.InvokeRequired)
+            {
+                statusLabel.Invoke(new Action<string>(UpdateStatus), message);
+                return;
+            }
+            statusLabel.Text = message;
+        }
+
+        private void UpdateProgress(int value)
+        {
+            if (progressBar.InvokeRequired)
+            {
+                progressBar.Invoke(new Action<int>(UpdateProgress), value);
+                return;
+            }
+            progressBar.Value = Math.Min(value, 100);
+        }
+
+        private void UpdateStats()
+        {
+            if (totalFilesLabel.InvokeRequired)
+            {
+                totalFilesLabel.Invoke(new Action(UpdateStats));
+                return;
+            }
+            totalFilesLabel.Text = totalFiles.ToString();
+            processedFilesLabel.Text = processedFiles.ToString();
+            createdFoldersLabel.Text = createdFolders.ToString();
+        }
+
+        private void OrganizeFiles()
+        {
+            try
+            {
+                UpdateStatus("Analyzing files...");
+                Log($"Initiating organization in: {selectedDirectory}");
+
+                // Diccionario para guardar los archivos encontrados por extensión
+                Dictionary<string, List<string>> filesByExtension = new Dictionary<string, List<string>>();
+                List<string> allFiles = new List<string>();
+
+                // Recorrer archivos
+                if (includeSubdirectories)
+                {
+                    allFiles.AddRange(Directory.GetFiles(selectedDirectory, "*.*", SearchOption.AllDirectories));
+                }
+                else
+                {
+                    allFiles.AddRange(Directory.GetFiles(selectedDirectory));
+                }
+
+                // Excluir el propio ejecutable
+                string exePath = Application.ExecutablePath;
+                allFiles.RemoveAll(file => file.Equals(exePath, StringComparison.OrdinalIgnoreCase));
+
+                // Actualizar total de archivos
+                totalFiles = allFiles.Count;
+                UpdateStats();
+                Log($"Found {totalFiles} files to process");
+
+                // Clasificar archivos por extensión
+                foreach (string filePath in allFiles)
+                {
+                    string extension = Path.GetExtension(filePath).TrimStart('.');
+
+                    if (useUppercase)
+                        extension = extension.ToUpper();
+                    else
+                        extension = extension.ToLower();
+
+                    // Si el archivo no tiene extensión, usar "SIN_EXTENSION"
+                    if (string.IsNullOrEmpty(extension))
+                        extension = useUppercase ? "NO_EXTENSION" : "NO_extension";
+
+                    if (!filesByExtension.ContainsKey(extension))
+                        filesByExtension[extension] = new List<string>();
+
+                    filesByExtension[extension].Add(filePath);
+                }
+
+                // Crear carpetas y mover archivos
+                processedFiles = 0;
+                createdFolders = 0;
+
+                foreach (var kvp in filesByExtension)
+                {
+                    string extension = kvp.Key;
+                    List<string> fileList = kvp.Value;
+                    string folderPath = Path.Combine(selectedDirectory, extension);
+
+                    // Crear la carpeta si no existe
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                        createdFolders++;
+                        UpdateStats();
+                        Log($"Folder created: {extension}");
+                    }
+
+                    // Mover archivos a la carpeta correspondiente
+                    foreach (string filePath in fileList)
+                    {
+                        string fileName = Path.GetFileName(filePath);
+                        string destination = Path.Combine(folderPath, fileName);
+
+                        // Si el archivo de destino ya existe, añadir un número
+                        if (File.Exists(destination))
+                        {
+                            string baseName = Path.GetFileNameWithoutExtension(fileName);
+                            string ext = Path.GetExtension(fileName);
+                            int counter = 1;
+                            while (File.Exists(Path.Combine(folderPath, $"{baseName}_{counter}{ext}")))
+                            {
+                                counter++;
+                            }
+                            destination = Path.Combine(folderPath, $"{baseName}_{counter}{ext}");
+                        }
+
+                        try
+                        {
+                            File.Move(filePath, destination);
+                            processedFiles++;
+                            UpdateStats();
+
+                            // Actualizar barra de progreso
+                            int progress = (int)((double)processedFiles / totalFiles * 100);
+                            UpdateProgress(progress);
+
+                            // Actualizar cada 10 archivos
+                            if (processedFiles % 10 == 0 || processedFiles == totalFiles)
+                            {
+                                UpdateStatus($"Processing... {processedFiles}/{totalFiles}");
+                            }
+
+                            // Registrar cada 20 archivos
+                            if (processedFiles % 20 == 0 || processedFiles == 1 || processedFiles == totalFiles)
+                            {
+                                Log($"Processed {processedFiles}/{totalFiles} files");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Log($"Error when moving {fileName}: {ex.Message}");
+                        }
+                    }
+                }
+
+                // Operación completada
+                UpdateStatus("Organization completed!");
+                Log("File organization successfully completed");
+                MessageBox.Show($"{processedFiles} files have been organized into {createdFolders} folders.", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                UpdateStatus("Error during organization");
+                Log($"ERROR: {ex.Message}");
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
